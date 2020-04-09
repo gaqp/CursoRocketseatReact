b0fcbe76bb6d9de8103fe2680666c17d101639b5
gaqp <gaqp@cin.ufpe.br>
2020-04-09 03:24:32 -0300

    Penultima aula

---
+++ src/pages/main/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api.js';
+
+import './styles.css';
+
+
+export default class Main extends Component {
+    state = {
+        products: []
+    };
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        this.setState({ products: response.data.docs });
+    }
+
+    render() {
+        const products = this.state.products;
+        return (
+            <div className="product-list">
+                {products.map(
+                    product => {
+                        return <article key={product._id}>
+                            <strong>{product.title}</strong>
+                            <p>{product.description}</p>
+                            <a href={product.url}>Acessar</a>
+                        </article>
+                    }
+                )
+                }
+            </div>
+        );
+    }
+}
